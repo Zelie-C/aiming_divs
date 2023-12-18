6f75f2fdeacc71d2658e3c9ceb4e578cd5d62244
Zelie Chemin <zelie.chemin@gmail.com>
2023-12-18 14:11:57 +0100

    ajout début timer

--- src/routes/Game.tsx
+++ src/routes/Game.tsx
@@ -1,5 +1,6 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import '../game.css'
+import { useNavigate } from 'react-router-dom'
 
 const randomizeLeftPosition = () => {
   return Math.floor(Math.random() * 100)
@@ -11,28 +12,50 @@ const randomizeTopPosition = () => {
 
 const Game = () => {
 
+  const navigate = useNavigate();
   const [clickCounter, setClickCounter] = useState<number>(0)
   const [leftPosition, setLeftPosition] = useState<number>(randomizeLeftPosition())
   const [topPosition, setTopPosition] = useState<number>(randomizeTopPosition())
+  const [startTime, setStartTime] = useState<number>(0)
+  const [endTime, setEndTime] = useState<number>(0)
 
-  // useEffect(
-  //   () => {
-  //     setLeftPosition(randomizeLeftPosition)
-  //     setTopPosition(randomizeTopPosition)
-  //   }, [leftPosition, topPosition]
-  // )
+  useEffect(() => {
+    const startDate = Date.now()
+    setStartTime(startDate)
+  }, [])
+
+  useMemo(() => {
+
+  })
+
+    setInterval(() => {
+    }, 1000)
+
+
 
   const handleDivClick = useCallback(() => {
-    setClickCounter(clickCounter + 1)
-    setLeftPosition(randomizeLeftPosition())
-    setTopPosition(randomizeTopPosition())
-  }, [clickCounter])
+    if (clickCounter < 9) {
+      setClickCounter(clickCounter + 1)
+      setLeftPosition(randomizeLeftPosition())
+      setTopPosition(randomizeTopPosition())
+    }
+    else {
+      const endDate = Date.now()
+      setEndTime(endDate)
+
+      navigate('/end')
+    }
+  }, [navigate, clickCounter])
+
+
 
   return (
     <>
       <div className='affichage-container'>
-        <div className="timer">Time : </div>
-        <div className='affichage'>{clickCounter} / 10</div>
+        <div className="col">
+          <div className="timer">Time : </div>
+          <div className='affichage'>{clickCounter} / 10</div>
+        </div>
       </div>
       <div className="game">
         <div className='aim' onClick={handleDivClick} style={{top: `${topPosition}%`, left: `${leftPosition}%`}}></div>
