1d004fd7bbfff5a1a0a7185dda0362234b9fe8e0
Zélie Chemin <zelie.chemin@gmail.com>
2023-12-18 15:37:20 +0100

    jeu fonctionnel manque le temps final

--- src/routes/End.tsx
+++ src/routes/End.tsx
@@ -1,6 +1,19 @@
+import { useNavigate } from "react-router-dom"
+
 const End = () => {
+
+  const navigate = useNavigate()
+  const handleRestartClick = () => {
+    localStorage.removeItem('time')
+    navigate('/game')
+  }
+  const time = localStorage.getItem('time')
+
   return (
     <>
+      <h1>Partie terminée</h1>
+      <p>Time: {time}</p>
+      <button onClick={handleRestartClick}>Restart</button>
     </>
   )
 }
--- src/routes/Game.tsx
+++ src/routes/Game.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import '../game.css'
 import { useNavigate } from 'react-router-dom'
 
@@ -16,22 +16,22 @@ const Game = () => {
   const [clickCounter, setClickCounter] = useState<number>(0)
   const [leftPosition, setLeftPosition] = useState<number>(randomizeLeftPosition())
   const [topPosition, setTopPosition] = useState<number>(randomizeTopPosition())
+  const [timelapse, setTimelapse] = useState<number>(0)
   const [startTime, setStartTime] = useState<number>(0)
   const [endTime, setEndTime] = useState<number>(0)
 
+
   useEffect(() => {
-    const startDate = Date.now()
-    setStartTime(startDate)
+    setStartTime(Date.now())
+    setInterval(() => {
+      setTimelapse(Math.random())
+    }, 120)
   }, [])
 
-  useMemo(() => {
-
-  })
-
-    setInterval(() => {
-    }, 1000)
-
-
+  const elapsedTime = useMemo(() => {
+    const time = (endTime - startTime) / 1000
+    return time
+  }, [])
 
   const handleDivClick = useCallback(() => {
     if (clickCounter < 9) {
@@ -42,18 +42,17 @@ const Game = () => {
     else {
       const endDate = Date.now()
       setEndTime(endDate)
-
+      localStorage.setItem('time', JSON.stringify(elapsedTime))
       navigate('/end')
     }
-  }, [navigate, clickCounter])
-
+  }, [navigate, clickCounter, endTime])
 
 
   return (
     <>
       <div className='affichage-container'>
         <div className="col">
-          <div className="timer">Time : </div>
+          <div className="timer">Time : {startTime ? (Date.now() - startTime) / 1000 : 0}</div>
           <div className='affichage'>{clickCounter} / 10</div>
         </div>
       </div>
