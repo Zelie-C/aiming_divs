179d6d84d80d4190ee67df096fb896821be27898
Zelie Chemin <zelie.chemin@gmail.com>
2023-12-18 13:10:48 +0100

    ajout style.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Game from './routes/Game'
 import Home from './routes/Home'
 import End from './routes/End'
+import './style.css'
 
 const router = createBrowserRouter([
   {
